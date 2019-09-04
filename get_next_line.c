4874fc685f019104f4ea0092a85fd288473036b8
Alexandra PAPPAS <apappas@c3r4s5.wethinkcode.co.za>
2019-09-04 09:19:38 +0200

    working gnl not fully tested

--- get_next_line.c
+++ get_next_line.c
@@ -6,109 +6,78 @@
 /*   By: apappas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/27 10:39:38 by apappas           #+#    #+#             */
-/*   Updated: 2019/07/30 14:27:17 by apappas          ###   ########.fr       */
+/*   Updated: 2019/09/02 14:07:39 by apappas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static void		to_free(t_listg **stat, int fd)
-{
-	t_listg	*find;
-	t_listg	*temp;
-
-	find = *stat;
-	if (*stat == NULL)
-		return ;
-	while (find->next->fd != fd)
-	{
-		if (find->next == NULL)
-			return ;
-		find = find->next;
-	}
-	temp = find->next;
-	find->next = find->next->next;
-	free(temp->content);
-	temp = NULL;
-}
-
-static char		*getfile(t_listg **stat, int fd)
+t_listg		*getfile(t_listg **stat, int fd)
 {
 	t_listg *find;
 	t_listg	*new;
 
+	if (!*stat)
+		*stat = NULL;
 	find = *stat;
 	while (find)
 	{
 		if (find->fd == fd)
-			return (find->content);
+			return (find);
 		find = find->next;
 	}
-	if (!*stat)
-		*stat = NULL;
 	new = (t_listg *)ft_memalloc(sizeof(t_listg));
 	new->content = ft_strnew(0);
 	new->fd = fd;
 	new->next = *stat;
 	*stat = new;
-	return (new->content);
+	return (new);
 }
 
-static char		*get_line(char **temp, t_listg **stat, int fd)
+char		*get_line(t_listg **temp)
 {
 	char	*ptr;
-	char	*line = NULL;
-	t_listg	*find;
+	char	*line;
+	t_listg	*node;
 
-	line = ft_strnew(0);
-	find = *stat;
-	(*stat)->content = getfile(stat, fd);
-	if ((ptr = ft_strchr(*temp, '\n')))
+	line = NULL;
+	node = *temp;
+	if ((ptr = ft_strchr((*temp)->content, '\n')))
 	{
 		*ptr = '\0';
-		line = ft_strcpy(line, *temp);
-		(*stat)->content = ft_strcpy((*stat)->content, (ptr + 1));
-		*stat = find;
-		ft_strclr(*temp);
-		return (line);	
+		line = ft_strdup((*temp)->content);
+		node->content = ft_strcpy(node->content, (ptr + 1));
+		return (line);
 	}
-	*stat = find;
-	line = ft_strcpy(line, *temp);
-	ft_strclr(*temp);
-	ft_strclr((*stat)->content);
+	line = ft_strdup((*temp)->content);
+	ft_strclr(node->content);
 	return (line);
 }
 
 int				get_next_line(const int fd, char **line)
 {
 	static t_listg	*stat;
-	char			*temp;
+	t_listg			*temp;
 	char			buf[BUFF_SIZE + 1];
 	int				readret;
+	char			*store;
 
 	if (fd < 0 || read(fd, buf, 0) < 0 || BUFF_SIZE <= 0)
 		return (-1);
 	*line = NULL;
-	if (!(temp = ft_strdup(getfile(&stat, fd))))
-		return (-1);
+	temp = getfile(&stat, fd);
 	while ((readret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		buf[readret] = '\0';
-		if (!(temp = ft_strjoin(temp, buf)))
-			return (-1);
+		store = ft_strdup(temp->content);
+		free(temp->content);
+		temp->content = ft_strjoin(store, buf);
+		free(store);
 		if (ft_strchr(buf, '\n'))
 			break ;
 	}
-	if (readret < 0)
-	{
-		to_free(&stat, fd);
-		return (-1);
-	}
-	if (readret < BUFF_SIZE && !ft_strlen(temp))
-	{
-		ft_strclr(temp);
-		return (0);
-	}
-	*line = get_line(&temp, &stat, fd);
+	if (readret == 0 && !ft_strlen(temp->content))
+			return (0);
+	*line = get_line(&temp);
 	return (1);
 }
--- main.c
+++ main.c
@@ -1,17 +1,48 @@
 #include "get_next_line.h"
+#include <fcntl.h>
 
-int	main(void)
+int		main(int argc, char **argv)
 {
-	int 	fd;
-	char 	*line;
-
-	fd = 0;
-	while(get_next_line(fd, &line) == 1)
+	int		fd1;
+	int		fd2;
+	char	*line1;
+	char	*line2;
+	int		ret1;
+	int		ret2;
+	int		i = 1;
+	
+	while (i <2)
+	{	
+	if (argc == 1)
+		fd1 = 0;
+	else if (argc == 2)
+		fd1 = open(argv[1], O_RDONLY);
+	else if (argc == 3)
 	{
-		ft_putendl(line);
-		free(line);
+		fd1 = open(argv[1], O_RDONLY);
+		fd2 = open(argv[2], O_RDONLY);
 	}
-	close(fd);
-	return(0);
-
+	else
+		return (2);
+	while ((ret2 = get_next_line(fd2, &line2) == 1))
+			{
+				
+				ft_putendl(line2);
+				free(line2);
+			}
+	while ((ret1 = get_next_line(fd1, &line1) == 1))
+		{
+			ft_putendl(line1);
+			free(line1);
+		}		
+		if (argc == 2)
+		close(fd1);
+		if (argc == 3)
+		{
+			close(fd1);
+			close(fd2);
+		}
+		i++;
+	}
+	return (0);
 }
